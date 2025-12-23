ec700bb02fef67601dd8156e86f99e93f3018529
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-12-23 09:12:42 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,27 @@
-print "lebronnnnnnnnnnnnnnnnnn"
+class zombie:
+    def __init__(self, speed, health, attack):
+        self.speed = speed
+        self.health = health
+        self.attack = attack
+
+    def regular (self):
+        speed = 5
+        health = 10
+        attack = 5
+
+    def fast ():
+        speed=10
+        health=7
+        attack:8
+
+    def tank():
+        speed=1
+        health=25
+        Attack:10
+
+    def boss():
+        speed=10
+        health=50
+        Attack:15
+
+print(f'Speed: {self.speed}')
