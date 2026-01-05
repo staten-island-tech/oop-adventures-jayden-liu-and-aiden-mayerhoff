d7991e6559e640aa2c1eedee377d8786de6b6900
Jaydenl6767 <Jaydenl321@nycstudents.net>
2026-01-05 09:23:01 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,27 +1,35 @@
-class zombie:
+class Zombie:
     def __init__(self, speed, health, attack):
         self.speed = speed
         self.health = health
         self.attack = attack
 
     def regular (self):
-        speed = 5
-        health = 10
-        attack = 5
+        self.speed = 5
+        self.health = 10
+        self.attack = 5
 
-    def fast ():
-        speed=10
-        health=7
-        attack:8
+    def fast (self):
+        self.speed=10
+        self.health=7
+        self.attack = 8
 
-    def tank():
-        speed=1
-        health=25
-        Attack:10
+    def tank(self):
+        self.speed = 5
+        self.health = 25
+        self.attack = 10
 
-    def boss():
-        speed=10
-        health=50
-        Attack:15
+    def boss(self):
+        self.speed = 10
+        self.health = 50
+        self.attack = 15
 
-print(f'Speed: {self.speed}')
+    def display(self):
+        print(f'speed: {self.speed}')
+        print (f'heath: {self.health}')
+        print(f'attack: {self.attack}') 
+
+z = Zombie(0,0,0)
+z.boss()
+z.display()
+         
