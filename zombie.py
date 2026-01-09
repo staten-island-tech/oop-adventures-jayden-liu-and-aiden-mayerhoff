1f99b153cd50e93bc7c4decdc95c5b8e2736f0ff
AidenCM1234 <aidenm124@nycstudents.net>
2026-01-09 08:48:50 -0500

    Update zombie.py

--- zombie.py
+++ zombie.py
@@ -28,27 +28,30 @@ class Zombie:
         print(f'speed: {self.speed}')
         print (f'heath: {self.health}')
         print(f'attack: {self.attack}') 
+    def random(self):
+        import random
+        random_number = random.randint(1,6)
+
+        if random_number <= 3: 
+            z.regular()
+            z.display()
+            print("You've encountered a Regular zombie!")
+
+        if random_number == 4 or random_number == 5:
+            z.fast()
+            z.display()
+            print("You've encountered a Fast zombie!")
+    
+        if random_number == 6:
+            z.tank()
+            z.display()
+            print("You've encountered a Tank zombie!")
+                
 
 z = Zombie(0,0,0)
 z.boss()
+z.random()
 
-import random
-random_number = random.randint(1,6)
-
-if random_number <= 3: 
-    z.regular()
-    z.display()
-    print("You've encountered a Regular zombie!")
-
-if random_number == 4 or random_number == 5:
-    z.fast()
-    z.display()
-    print("You've encountered a Fast zombie!")
-    
-if random_number == 6:
-    z.tank()
-    z.display()
-    print("You've encountered a Tank zombie!")
 
 """while z.speed < p.speed:
     z.health = z.health - p.attack 
